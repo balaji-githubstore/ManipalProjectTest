4241fe49d1927e0103f9de7fe50dc76d64f52b33
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-10-27 10:20:24 +0530

    test

--- src/test/java/com/manipal/basetest/BaseTest.java
+++ src/test/java/com/manipal/basetest/BaseTest.java
@@ -12,7 +12,7 @@ public class BaseTest {
 	public void testMethod()
 	{
 		//WebDriver driver = new ChromeDriver();
-		System.out.println("Test Data 123");
+		System.out.println("Test Data 12345454545");
 	}
 
 }
