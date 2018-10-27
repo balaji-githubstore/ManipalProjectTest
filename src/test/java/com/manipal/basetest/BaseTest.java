8a498c33aae2c1c001abfb566109d9a648be9abf
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-10-27 09:58:15 +0530

    firstcommit

---
+++ src/test/java/com/manipal/basetest/BaseTest.java
@@ -0,0 +1,18 @@
+package com.manipal.basetest;
+
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseTest {
+	
+	@Test
+	public void testMethod()
+	{
+		//WebDriver driver = new ChromeDriver();
+		System.out.println("Test Data");
+	}
+
+}
